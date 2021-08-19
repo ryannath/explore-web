31148d61976be1424f9b48d4aa9341a3c0e135c9
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2021-08-19 22:01:12 +1000

    Implemented barebone Hero to Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Hero />
     </div>
   );
 }
