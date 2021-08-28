d7d4b07dc3748511a25321102cf8a15d521586eb
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-29 00:40:07 +1000

    Implemented content to Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,59 @@
+import { Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   heroContainer: {
-    backgroundImage: 'url("landscape-eberhard-grossgasteiger.jpg")',
-    backgroundSize: 'cover',
     height: '100vh',
+  },
+
+  container: {
+    display: 'flex',
+    position: 'relative',
+    top: '18%'
+  },
+
+  content: {
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    borderRadius: 5,
+    padding: '2rem 4rem',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    backdropFilter: 'blur(16px) saturate(1.2)',
+    textAlign: 'center',
+
+    '& h1': {
+      fontFamily: "Qwigley, cursive",
+      fontSize: 100,
+      paddingBottom: '1rem'
+    },
+
+    '& p': {
+      fontSize: '1.2rem'
+    }
   }
 });
 
-const Hero = () => {
+
+
+const Hero = ({ image }) => {
   const classes = useStyles();
+  const content = (
+    <Box className={classes.container}>
+      <Box className={classes.content}>
+        <Typography variant='h1'>
+          Petualang
+        </Typography>
+        <Typography>
+        Explore websites to inspire your design
+        </Typography>
+      </Box>
+    </Box>
+  )
+
   return (
-    <Box className={classes.heroContainer}>
-      {/* Put hero content here */}
+    <Box className={classes.heroContainer} style={{backgroundImage: `url(${image})`, backgroundSize: 'cover'}}>
+      {content}
     </Box>
 
   )
