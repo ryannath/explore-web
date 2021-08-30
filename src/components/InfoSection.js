7dc92c8140ea6c5c1b7823be7a2035c0ada43ae0
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-30 18:13:04 +1000

    Added Info section according to Figma design, added card for description, leaf svg, made somewhat responsive

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,40 +1,123 @@
-import { makeStyles } from '@material-ui/core'
-import Typography from '@material-ui/core/Typography'
-import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { ReactComponent as Plants } from './Plants.svg';
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles(theme => ({
+  // Section root, relative for background image positioning
   root: {
+    position: 'relative',
+    overflow: 'hidden',
+    // padding to separate from other sections
     paddingTop: '5rem',
     paddingBottom: '5rem',
+  },
+
+  // Leaves decorations in the background
+  leaveDecor: {
+    position: 'absolute',
+    right: 0,
+    top: '10vh',
+    width: '30%',
+    zIndex: -1,
+
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+      top: '-4vh',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '120%',
+      top: '-4vh',
+    },
+  },
+  
+  // Not the actual card itself, just the container, positions
+  // the card
+  cardContainer: {
+    paddingLeft: '15%',
+    paddingRight: '15%',
+    // *** padding is included in width size
+    width: '75%',
+
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      padding: '0px 5vw'  
+    },
+  },
+
+  // The actual card itself
+  sideCard: {
+    padding: '3rem 3rem 8rem 3rem',
+
+    [theme.breakpoints.down('xs')]: {
+      padding: '3rem 1rem 4rem 1rem'  
+    }
+  },
+
+  sideCardTitle: {
+    fontSize: 36,
+    marginBottom: '1rem',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 36,
+    }
+  },
+
+  sideCardSubTitle: {
+    fontSize: 28,
+    color: 'gray',
+    letterSpacing: 5,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 20,
+    }
+  },
+
+  primaryParagraph: {
+    marginBottom: 10,
+  },
+
+  cardParagraph: {
+    maxWidth: 750,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 14,
+    }
   }
-});
+}));
 
 const InfoSection = () => {
   const classes = useStyles();
 
+  const card = (
+    <Card className={classes.sideCard}>
+      <Typography component='h2' className={classes.sideCardTitle}>
+        Petualang
+        <Typography component='span' className={classes.sideCardSubTitle}> •&nbsp;explorer </Typography>
+      </Typography>
+      <Box>
+        <Typography className={classes.primaryParagraph + ' ' + classes.cardParagraph}>
+        Design like all things requires practice and brave experimentations. Whether it be illustration or web design, there is a variety of websites to draw inspirations from.
+        </Typography>
+        <Typography className={classes.cardParagraph}>
+          Petualang 
+          <Typography color='secondary' component='span' className={classes.cardParagraph}> connects you </Typography>
+          to the most popular websites that feature design oriented
+          content to accelerate the exploration phase of design.
+        </Typography>
+      </Box>
+    </Card>
+  );
+
   return (
-    <Container className={classes.root}>
-      <Typography variant='h3' component='h2'>
-        About
-      </Typography>
-      <Typography variant='body1'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum.
-      </Typography>
-      <Typography>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum.
-      </Typography>
-    </Container>
-  )
+    <Box className={classes.root} id='Info'>
+      <Box className={classes.cardContainer}>
+        {card}
+      </Box>
+      <Plants className={classes.leaveDecor}/>
+    </Box>
+  );
 }
 
-export default InfoSection
+export default InfoSection;
