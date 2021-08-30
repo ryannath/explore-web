bcb64fdd0f53489248886d5bb5ae621334161bcd
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-31 00:08:02 +1000

    Changed svg height to 100% to fill the section correctly, prevent preservation of aspect ratio

--- src/components/FeaturedBackground.js
+++ src/components/FeaturedBackground.js
@@ -3,7 +3,8 @@ const FeaturedBackground = ({ className }) => {
     <svg className={className}
       xmlns="http://www.w3.org/2000/svg"
       width="1920"
-      height="1577"
+      height="100%"
+      preserveAspectRatio="none"
       fill="none"
       viewBox="0 0 1920 1577"
     >
