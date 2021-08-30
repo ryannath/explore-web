9c326ac02214f645d06e106b36c43d37a110d88b
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-30 23:34:50 +1000

    Moved Cards in the featured section into their own component

---
+++ src/components/FeatureCard.js
@@ -0,0 +1,34 @@
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  cardContent: {
+    minHeight: 300
+  },
+
+  media: {
+    height: 200,
+  },
+
+});
+
+const FeatureCard = ({ image, desc }) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={image}/>
+        <CardContent className={classes.cardContent}>
+          <Typography>
+            {desc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>  )
+}
+
+export default FeatureCard
