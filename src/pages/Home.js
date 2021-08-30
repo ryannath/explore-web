7996067b4abfa7a44702a1a7705999fa985631fa
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-30 18:13:25 +1000

    Linked call to action button to info section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      <Hero image={'li-background.png'} />
+      <Hero image={'li-background.png'} callToActionRef='#Info' />
       <InfoSection />
       <Content />
     </>
