92e96ad296cbe0063d5028dff4537f95af24e83b
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-30 15:21:53 +1000

    Text on Hero will respond to smaller screen size, added a call to action button, and added some comments for easier maintenance

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
-import { ReactComponent as HomeLogo } from './homeIcon.svg'
+import { ReactComponent as HomeLogo } from './HomeIcon.svg'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 const useStyles = makeStyles((theme) => ({
