7418e4db0b83601474dd9a9e4f57ca3cbbd59e07
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-08-30 23:36:59 +1000

    Implemented Figma design, specifically added information on each cards, and positioned background svg

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,83 +1,95 @@
-import CardActionArea from '@material-ui/core/CardActionArea'
-import Card from '@material-ui/core/Card'
 import Grid from '@material-ui/core/Grid'
-import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
-import CardMedia from '@material-ui/core/CardMedia';
+import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core'
+// import { ReactComponent as FeaturedBackground } from './FeaturedBackground.svg';
+import FeaturedBackground from './FeaturedBackground';
+import Wave from './Wave';
+import FeatureCard from './FeatureCard'
+// import ReactLogo from './Wave.svg';
 
 const useStyles = makeStyles({
   root: {
     paddingBottom: '5rem',
+    position: 'relative',
+    overflow: 'hidden',
   },
+
+  background: {
+    zIndex: -1,
+    position:'absolute',
+    height: 'inherit',
+    top: '5vh',
+  },
+
+  section: {
+    paddingTop: '30vh',
+    paddingLeft: '15%',
+    paddingRight: '15%',
+  },
+
   media: {
     height: 140,
   },
+
+  wave: {
+    position: 'absolute',
+    zIndex: -2,
+    top: 0,
+
+  }
 });
+
+
+const featured = [
+  {
+    img: 'dribbble-logo.png',
+    desc: `Dribbble showcases a wide variety of design work from
+    illustrations, mobile app design, website design, and logo.
+    Users may also follow certain creators they are interested in, or
+    create their own portfolio to showcase the work they've done`
+  },
+  {
+    img: 'behance-logo.jpeg',
+    desc: `Behance is a website by Adobe that focuses on portfolio display.
+    In many ways, it is simlar to dribbble, it has a multitude of design work
+    in different forms and profiles to follow. The vertical layouting of each
+    work makes it more similar to websites`
+  },
+  {
+    img: 'uiiiuiii-logo.jpg',
+    desc: `Uiiiuiii provides a large number of tutorials in the creative field,
+    including those on Photoshop and Illustrator. It also provides a number of website design
+    to be taken as inspiration. The website and its content are in Chinese, however, this can
+    be overcomed using Chrome's translate page`
+  },
+]
 
 
 const Content = () => {
   const classes = useStyles();
+  
   return (
-    <Container className={classes.root}>
-      <Typography variant='h3' component='h2'>
-        Featured
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardActionArea>
-              <CardMedia className={classes.media} image='landscape-eberhard-grossgasteiger.jpg'/>
-              <CardContent>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                  dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+    <Box className={classes.root}>
+      <FeaturedBackground className={classes.background}/>
+      <Wave className={classes.wave}/>
+      <Box className={classes.section}>
+        <Typography variant='h4' component='h2'>
+          Featured
+        </Typography>
+        <Grid container spacing={2}>
+          {featured.map((item) => (
+            <Grid item xs={12} md={4}>
+              <FeatureCard
+                image={item.img}
+                desc={item.desc}
+              />
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardActionArea>
-              <CardMedia className={classes.media} image='landscape-eberhard-grossgasteiger.jpg'/>
-              <CardContent>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                  dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardActionArea>
-              <CardMedia className={classes.media} image='landscape-eberhard-grossgasteiger.jpg'/>
-              <CardContent>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                  dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
-    </Container>
+      </Box>
+    </Box>
   )
 }
 
