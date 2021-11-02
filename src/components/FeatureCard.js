a6dfdeb8d6ace68967003e9c1073e6f78d9f246a
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-11-03 00:03:03 +1100

    Changed card stylings

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -5,25 +5,41 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '0rem 0rem 2rem 0rem',
+  },
+
   cardContent: {
     minHeight: 300
   },
 
   media: {
-    height: 200,
+    height: '12rem',
+  
+    [theme.breakpoints.up('sm')]: {
+      height: '18rem',
+    },
+
+    [theme.breakpoints.up('md')]: {
+      height: '200px',
+    }
   },
 
-});
+  cardText: {
+    fontSize: '1rem'
+  }
+
+}));
 
 const FeatureCard = ({ image, desc, url }) => {
   const classes = useStyles();
   return (
-    <Card>
+    <Card className={classes.root}>
       <CardActionArea onClick={()=>window.open(url, '_blank')}>
         <CardMedia className={classes.media} image={image}/>
         <CardContent className={classes.cardContent}>
-          <Typography>
+          <Typography className={classes.cardText}>
             {desc}
           </Typography>
         </CardContent>
