e9e42d9f79209c713da97cdf1b9947e1ff94e235
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-11-03 09:30:02 +1100

    Include Footer in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import InfoSection from '../components/InfoSection';
 import Content from '../components/Content'
+import Footer from '../components/Footer';
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero callToActionRef='#Info' />
       <InfoSection />
       <Content />
+      <Footer />
     </>
   )
 }
