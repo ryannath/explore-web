3d17b18e3ac0186d1bd4971c7e83ced66d66d6e7
Ryan Nathan Mulyadi <ryan.nathan@hotmail.com>
2021-11-02 21:42:13 +1100

    Changed design of InfoSection from the card to the several icons layout

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,121 +1,78 @@
 import { makeStyles } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { ReactComponent as Plants } from './Plants.svg';
-
+import LogoScience from './SVG/LogoScience';
+import LogoDesign from './SVG/LogoDesign';
+import LogoNature from './SVG/LogoNature'
+import LogoArchitecture from './SVG/LogoArchitecture';
+import Icon from './Icon'
 
 const useStyles = makeStyles(theme => ({
   // Section root, relative for background image positioning
   root: {
-    position: 'relative',
     overflow: 'hidden',
+    margin: '5rem 0rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     // padding to separate from other sections
-    paddingTop: '5rem',
-    paddingBottom: '5rem',
+
   },
 
-  // Leaves decorations in the background
-  leaveDecor: {
-    position: 'absolute',
-    right: 0,
-    top: '10vh',
-    width: '30%',
-    zIndex: -1,
-
-    [theme.breakpoints.down('md')]: {
-      width: '100%',
-      top: '-4vh',
-    },
-    [theme.breakpoints.down('xs')]: {
-      width: '120%',
-      top: '-4vh',
-    },
-  },
-  
   // Not the actual card itself, just the container, positions
   // the card
-  cardContainer: {
-    paddingLeft: '15%',
-    paddingRight: '15%',
-    // *** padding is included in width size
-    width: '75%',
+  contentContainer: {
 
-    [theme.breakpoints.down('md')]: {
-      width: '100%',
-    },
-    [theme.breakpoints.down('xs')]: {
-      padding: '0px 5vw'  
-    },
+    maxWidth: '40rem',
+    textAlign: 'center',
+
+    // [theme.breakpoints.down('md')]: {
+    //   width: '100%',
+    // },
+    // [theme.breakpoints.down('xs')]: {
+    //   padding: '0px 5vw'  
+    // },
   },
 
-  // The actual card itself
-  sideCard: {
-    padding: '3rem 3rem 8rem 3rem',
-
-    [theme.breakpoints.down('xs')]: {
-      padding: '3rem 1rem 4rem 1rem'  
-    }
+  firstParagraph: {
+    paddingBottom: '1rem',
   },
 
-  sideCardTitle: {
-    // fontSize: 36,
-    marginBottom: '1rem',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 36,
-    }
+  secondParagraph: {
+    paddingBottom: '3rem'
   },
 
-  sideCardSubTitle: {
-    fontSize: 28,
-    color: 'gray',
-    letterSpacing: 5,
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 20,
-    }
-  },
-
-  primaryParagraph: {
-    marginBottom: 10,
-  },
-
-  cardParagraph: {
-    maxWidth: 750,
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 14,
-    }
+  iconContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: '60%'
   }
 }));
 
 const InfoSection = () => {
   const classes = useStyles();
 
-  const card = (
-    <Card className={classes.sideCard}>
-      <Typography variant= 'h4' component='h2' className={classes.sideCardTitle}>
-        Petualang
-        <Typography component='span' className={classes.sideCardSubTitle}> •&nbsp;explorer </Typography>
-      </Typography>
-      <Box>
-        <Typography className={classes.primaryParagraph + ' ' + classes.cardParagraph}>
-        Design like all things requires practice and brave experimentations. Whether it be illustration or web design, there is a variety of websites to draw inspirations from.
-        </Typography>
-        <Typography className={classes.cardParagraph}>
-          Petualang 
-          <Typography color='secondary' component='span' className={classes.cardParagraph}> connects you </Typography>
-          to the most popular websites that feature design oriented
-          content to accelerate the exploration phase of design.
-        </Typography>
-      </Box>
-    </Card>
-  );
-
   return (
     <Box className={classes.root} id='Info'>
-      <Box className={classes.cardContainer}>
-        {card}
+      <Box className={classes.contentContainer}>
+        <Typography variant='h4' component='h2'>PETUALANG - EXPLORER</Typography>
+        <Typography className={classes.firstParagraph}>
+          Petualang
+          <Typography style={{color: '#0D40DD'}}component='span'> connects you </Typography>
+          to the grandeur of life</Typography>
+        <Typography className={classes.secondParagraph}>
+          Find ways to learn more of the topics you know and love,
+          discover fascinating subjects to be passionate about,
+          or simply explore the vast collective knowledge and experience of humanity
+        </Typography>
       </Box>
-      <Plants className={classes.leaveDecor}/>
+      <Box className={classes.iconContainer}>
+        <Icon text="Science"><LogoScience/></Icon>
+        <Icon text="Design"><LogoDesign/></Icon>
+        <Icon text="Nature"><LogoNature/></Icon>
+        <Icon text="Architecture"><LogoArchitecture/></Icon>
+      </Box>
     </Box>
   );
 }
